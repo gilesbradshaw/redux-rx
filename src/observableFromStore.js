5094afe3d7cad437c80094fae174bfa04518c54b
Giles <giles.bradshaw@sigyl.com>
2016-06-30 00:13:39 +0100

    ..

--- src/observableFromStore.js
+++ src/observableFromStore.js
@@ -2,6 +2,6 @@ import { Observable } from 'rxjs';
 
 export default function observableFromStore(store) {
   return Observable.create(observer =>
-    store.subscribe(() => observer.onNext(store.getState()))
+    store.subscribe(() => observer.next(store.getState()))
   );
 }
